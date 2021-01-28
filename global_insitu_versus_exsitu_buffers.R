abdfffb058d4f55f5444f9d2130bff28e6e265ea
Emily Beckman <ebeckman@mortonarb.org>
2021-01-28 10:43:22 -0600

    ready for practice

--- global_insitu_versus_exsitu_buffers.R
+++ global_insitu_versus_exsitu_buffers.R
@@ -46,8 +46,8 @@
 ### LIBRARIES ###
 #################
 
-my.packages <- c("leaflet","raster","sp","rgeos","dplyr","rgdal","Polychrome")
-	"cleangeo","RColorBrewer","smoothr")
+my.packages <- c("leaflet","raster","sp","rgeos","plyr","dplyr","rgdal",
+	"Polychrome","cleangeo","RColorBrewer","smoothr")
 #install.packages(my.packages) # turn on to install current versions
 lapply(my.packages, require, character.only=TRUE)
 
@@ -118,6 +118,20 @@ format.cell <- function(ex_result,in_result,final_result){
 	return(cell)
 }
 
+# clip points by boundary so only in target area
+# (helpful if focusing on one country/region)
+clip.by.boundary <- function(pts,pt_proj,boundary){
+	# select coordinate columns
+	latlong <- pts %>% select(decimalLongitude,decimalLatitude)
+	# turn occurrence point data into a SpatialPointsDataFrame
+	spatial_pts <- SpatialPointsDataFrame(latlong, pts, proj4string = pt_proj)
+	# clip by boundary created earlier
+	spatial_pts <- spatial_pts[boundary, ]
+	# keep just the data (not the spatial info you added)
+	pts_new <- spatial_pts@data
+	return(pts_new)
+}
+
 ################################################################################
 ################################################################################
 ## Set things up
@@ -141,7 +155,7 @@ world_countries <- readOGR(file.path(poly_dir,"UIA_World_Countries_Boundaries-sh
 	##	there is a self-intersection error when trying the aggregate function for
 	##	the aea projection using all countries; tried clgeo_Clean and did not fix
 sort(unique(world_countries@data$ISO))
-target_iso <- c("CN")
+target_iso <- c("CN","JP")
 target_countries <- world_countries[world_countries@data$ISO %in% target_iso,]
 	## create polygon for clipping buffers later, one in each projection
 target_countries.wgs <- spTransform(target_countries,wgs.proj)
@@ -185,7 +199,7 @@ triangle_lg <- makeIcon(iconUrl = "https://www.freeiconspng.com/uploads/triangle
 
 ### CREATE LIST OF TARGET SPECIES
 
-target_sp <- c("Acer_griseum","Acer_x")
+target_sp <- c("Acer_griseum","Acer_pycnanthum")
 ## select species to work with now
 sp <- 1
 
@@ -196,52 +210,45 @@ insitu <- read.csv(file.path(pts_dir,paste0(target_sp[sp],
 	"_distribution_points_2020.csv")),na.strings=c("","NA"),
 	stringsAsFactors = F)
 str(insitu)
-## change column names or remove columns as needed; should have at least
-##	"decimalLatitude","decimalLongitude","species_name"
+## change column names or remove columns as needed; need at least
+##	"decimalLatitude" and "decimalLongitude"
 insitu <- insitu %>%
-	rename(decimalLatitude = latitude,decimalLongitude = longitude) %>%
-	filter(category == "in_situ") %>%
-	select(-category)
+	rename(decimalLatitude = Latitude, decimalLongitude = Longitude) %>%
+	filter(Category == "in_situ")
 ## if desired, can clip points by boundary so only in target area
 ## (helpful if focusing on one country/region)
-	## select coordinate columns
-latlong <- insitu %>% select(decimalLongitude,decimalLatitude)
-	## turn occurrence point data into a SpatialPointsDataFrame
-spatial_pts <- SpatialPointsDataFrame(latlong, insitu, proj4string = wgs.proj)
-	## clip by boundary created earlier
-spatial_pts <- spatial_pts[boundary.wgs, ]
-	## keep just the data (not the spatial info you added)
-insitu <- spatial_pts@data
+insitu <- clip.by.boundary(insitu,wgs.proj,boundary.wgs)
 str(insitu)
 
 ## read in ex situ wild locality points
 exsitu <- read.csv(file.path(pts_dir,paste0(target_sp[sp],
-	"_distribution_points_2020.csv")),na.strings=c("","NA"),
-	stringsAsFactors = F)
-str(insitu)
+	"_ALL_POSTGEO.csv")),na.strings=c("","NA"),stringsAsFactors = F)
+str(exsitu)
 ## change column names or remove columns as needed; should have at least
-##	"decimalLatitude","decimalLongitude","species_name"
-insitu <- insitu %>%
-	rename(decimalLatitude = latitude,decimalLongitude = longitude) %>%
-	filter(category == "in_situ") %>%
-	select(-category)
-
-
-## remove points that are very close together
+##	"decimalLatitude","decimalLongitude","num_indiv"
+exsitu <- exsitu %>%
+	rename(decimalLatitude = lat_dd, decimalLongitude = long_dd) %>%
+	select(UID,inst_short,prov_type,gps_det,decimalLatitude,decimalLongitude,
+		num_indiv)
+str(exsitu)
+unique(exsitu$num_indiv)
+## remove points that are very close together, so can see mapped better
 ## you can change number of digits to remove less
-insitu$lat_round <- round(as.numeric(insitu$decimalLatitude),digits=1)
-insitu$long_round <- round(as.numeric(insitu$decimalLongitude),digits=1)
-insitu <- insitu %>%
-	  group_by(lat_round,long_round,category) %>%
-	  #mutate(num_indiv = sum(as.numeric(num_indiv))) %>%
-	  ungroup() %>%
-	  distinct(lat_round,long_round,category,.keep_all=T) %>%
-		select(-lat_round,-long_round)
-
-## create layer of ex situ points only
-exsitu <- insitu[insitu$category == "ex_situ", ]
-#exsitu$num_indiv <- as.numeric(exsitu$num_indiv)
-nrow(exsitu)
+exsitu$lat_round <- round(as.numeric(exsitu$decimalLatitude),digits=2)
+exsitu$long_round <- round(as.numeric(exsitu$decimalLongitude),digits=2)
+exsitu <- exsitu %>%
+	filter(!is.na(lat_round)) %>%
+	group_by(lat_round,long_round) %>%
+	mutate(num_indiv = sum(as.numeric(num_indiv))) %>%
+	ungroup() %>%
+		## add things here if you want extra columns preserved; !! could also
+		##	collapse institution names so they are all listed in popup
+	distinct(lat_round,long_round,.keep_all=T) %>%
+	select(-lat_round,-long_round)
+## if desired, can clip points by boundary so only in target area
+## (helpful if focusing on one country/region)
+exsitu <- clip.by.boundary(exsitu,wgs.proj,boundary.wgs)
+str(exsitu)
 ## optionally, remove any bad ex situ points manually by ID number
 #if(exsitu$species_name_acc[1] == "Juglans californica"){
 #	exsitu <- exsitu %>% filter(UID != "id00092049" & UID != "id00091728")
@@ -250,13 +257,18 @@ nrow(exsitu)
 #}
 #sum(exsitu$num_indiv)
 ## split by number of individuals, to use different sized symbol for each
-#exsitu1 <- exsitu %>% arrange(num_indiv) %>% filter(num_indiv <= 10)
-#exsitu2 <- exsitu %>% arrange(num_indiv) %>% filter(num_indiv > 10 & num_indiv < 30)
-#exsitu3 <- exsitu %>% arrange(num_indiv) %>% filter(num_indiv >= 30)
+## change as needed to get categories you want
+exsitu1 <- exsitu %>% arrange(num_indiv) %>% filter(num_indiv <= 1)
+exsitu2 <- exsitu %>% arrange(num_indiv) %>% filter(num_indiv > 2 & num_indiv < 4)
+exsitu3 <- exsitu %>% arrange(num_indiv) %>% filter(num_indiv >= 5)
+
+## add ex situ points to in situ points
+insitu <- rbind.fill(insitu,exsitu)
 
 ### CREATE MAP
 
 ## create buffers for visualization
+## change number to get different buffer size; e.g., 20000 = 20km
 insitu_buff_wgs <- create.buffers(insitu,20000,wgs.proj,wgs.proj,boundary.wgs)
 exsitu_buff_wgs <- create.buffers(exsitu,20000,wgs.proj,wgs.proj,boundary.wgs)
 
@@ -269,13 +281,13 @@ map <- leaflet(options = leafletOptions(maxZoom = 9)) %>%
 	addControl(paste0("<b>",gsub("_"," ",target_sp[sp])), position = "topright") %>%
 	## Ecoregions
 	addPolygons(
-		data = ecoregions_clip.wgs,
+		data = ecoregions_clip.wgs, label = ~ECO_NAME,
 		fillColor = ~eco_pal(ecoregions_clip.wgs@data$ECO_ID),
 		fillOpacity = 0.9, color = "#757575", weight = 1.5, opacity = 0.8) %>%
 	## Country outlines
-	addPolygons(
-		data = target_countries.wgs,
-		fillColor = "transparent", weight = 1.5, opacity = 0.5, color = "black") %>%
+	#addPolygons(
+	#	data = target_countries.wgs, label = ~COUNTRY,
+	#	fillColor = "transparent", weight = 1.5, opacity = 0.5, color = "black") %>%
 	## Buffers
 		## In situ
 	addPolygons(
@@ -290,15 +302,23 @@ map <- leaflet(options = leafletOptions(maxZoom = 9)) %>%
 		weight = 1.3, color = "white", opacity = 0,
 		smoothFactor = 0) %>%
 	## Ex situ points
-  addMarkers(data = exsitu,
-		lng = ~decimalLongitude, lat = ~decimalLatitude, icon = triangle_sm) %>%
-  #addMarkers(data = exsitu2,
-	#	lng = ~decimalLongitude, lat = ~decimalLatitude, icon = triangle_md) %>%
-  #addMarkers(data = exsitu3,
-	#	lng = ~decimalLongitude, lat = ~decimalLatitude, icon = triangle_lg) %>%
+  addMarkers(data = exsitu1,
+		lng = ~decimalLongitude, lat = ~decimalLatitude, icon = triangle_sm,
+		popup = exsitu1$inst_short) %>%
+	addMarkers(data = exsitu2,
+		lng = ~decimalLongitude, lat = ~decimalLatitude, icon = triangle_md,
+		popup = exsitu2$inst_short) %>%
+  addMarkers(data = exsitu3,
+		lng = ~decimalLongitude, lat = ~decimalLatitude, icon = triangle_lg,
+		popup = exsitu3$inst_short) %>%
+	## (optional) In situ points
+	addCircleMarkers(data = insitu,
+		lng = ~decimalLongitude, lat = ~decimalLatitude,
+		color = "white", radius = 3, fillOpacity = 1, stroke = F) %>%
+	## Add scale bar and set view
 	addScaleBar(position = "bottomright",
-		options = scaleBarOptions(maxWidth = 150)) #%>%
-	#setView(-98, 40, zoom = 5)
+		options = scaleBarOptions(maxWidth = 150)) %>%
+	setView(104, 32, zoom = 5)
 map
 
 ## save map
@@ -309,11 +329,6 @@ htmlwidgets::saveWidget(map, file.path(output_dir,paste0(target_sp[sp],"_leaflet
 ################################################################################
 ## Calculate geographic and ecological coverage of ex situ collections
 ################################################################################
-
-### CREATE LIST OF TARGET SPECIES
-
-target_sp <- c("Acer griseum")
-sp <- 1
 
 ### START SUMMARY TABLE
 
@@ -326,46 +341,92 @@ summary_tbl <- data.frame(species = "start",
 
 ### CYCLE THROUGH TARGET SPECIES TO CALCULATE EX SITU COVERAGE
 
-##
 for(sp in 1:length(target_sp)){
-##
 
 	# print progress
   cat("\tStarting ", target_sp[sp])
 
 	### READ IN AND PREP POINT DATA
 
+	## RIGHT NOW THIS IS JUST COPIED FROM ABOVE; WAY TO SHORTEN/NOT REPEAT?
+	## read in wild in situ occurrence points
+	insitu <- read.csv(file.path(pts_dir,paste0(target_sp[sp],
+		"_distribution_points_2020.csv")),na.strings=c("","NA"),
+		stringsAsFactors = F)
+	## change column names or remove columns as needed; need at least
+	##	"decimalLatitude" and "decimalLongitude"
+	insitu <- insitu %>%
+		rename(decimalLatitude = Latitude, decimalLongitude = Longitude) %>%
+		filter(Category == "in_situ")
+	## if desired, can clip points by boundary so only in target area
+	## (helpful if focusing on one country/region)
+	insitu <- clip.by.boundary(insitu,wgs.proj,boundary.wgs)
 
+	## read in ex situ wild locality points
+	exsitu <- read.csv(file.path(pts_dir,paste0(target_sp[sp],
+		"_ALL_POSTGEO.csv")),na.strings=c("","NA"),stringsAsFactors = F)
+	## change column names or remove columns as needed; should have at least
+	##	"decimalLatitude","decimalLongitude","num_indiv"
+	exsitu <- exsitu %>%
+		rename(decimalLatitude = lat_dd, decimalLongitude = long_dd) %>%
+		select(UID,inst_short,prov_type,gps_det,decimalLatitude,decimalLongitude,
+			num_indiv)
+	unique(exsitu$num_indiv)
+	## remove points that are very close together, so can see mapped better
+	## you can change number of digits to remove less
+	exsitu$lat_round <- round(as.numeric(exsitu$decimalLatitude),digits=2)
+	exsitu$long_round <- round(as.numeric(exsitu$decimalLongitude),digits=2)
+	exsitu <- exsitu %>%
+		filter(!is.na(lat_round)) %>%
+		group_by(lat_round,long_round) %>%
+		mutate(num_indiv = sum(as.numeric(num_indiv))) %>%
+		ungroup() %>%
+			## add things here if you want extra columns preserved; !! could also
+			##	collapse institution names so they are all listed in popup
+		distinct(lat_round,long_round,.keep_all=T) %>%
+		select(-lat_round,-long_round)
+	## if desired, can clip points by boundary so only in target area
+	## (helpful if focusing on one country/region)
+	exsitu <- clip.by.boundary(exsitu,wgs.proj,boundary.wgs)
+	## optionally, remove any bad ex situ points manually by ID number
+	#if(exsitu$species_name_acc[1] == "Juglans californica"){
+	#	exsitu <- exsitu %>% filter(UID != "id00092049" & UID != "id00091728")
+	#} else if (exsitu$species_name_acc[1] == "Juglans major"){
+	#	exsitu <- exsitu %>% filter(UID != "id00092280")
+	#}
+	#sum(exsitu$num_indiv)
 
+	## add ex situ points to in situ points
+	insitu <- rbind.fill(insitu,exsitu)
 
 	### CALCULATE EX SITU COVERAGE
 
 	## Geographic coverage
-		# 20 km buffers
-	geo_20_insitu <- calc.buff.area(insitu,20000,wgs.proj,aea.proj,us_boundary.aea)
-	geo_20_exsitu <- calc.buff.area(exsitu,20000,wgs.proj,aea.proj,us_boundary.aea)
+		## 20 km buffers
+	geo_20_insitu <- calc.buff.area(insitu,20000,wgs.proj,aea.proj,boundary.aea)
+	geo_20_exsitu <- calc.buff.area(exsitu,20000,wgs.proj,aea.proj,boundary.aea)
 	geo_20_percent <- (geo_20_exsitu/geo_20_insitu)*100
-		# 50 km buffers
-	geo_50_insitu <- calc.buff.area(insitu,50000,wgs.proj,aea.proj,us_boundary.aea)
-	geo_50_exsitu <- calc.buff.area(exsitu,50000,wgs.proj,aea.proj,us_boundary.aea)
+		## 50 km buffers
+	geo_50_insitu <- calc.buff.area(insitu,50000,wgs.proj,aea.proj,boundary.aea)
+	geo_50_exsitu <- calc.buff.area(exsitu,50000,wgs.proj,aea.proj,boundary.aea)
 	geo_50_percent <- (geo_50_exsitu/geo_50_insitu)*100
-		# 100 km buffers
-	geo_100_insitu <- calc.buff.area(insitu,100000,wgs.proj,aea.proj,us_boundary.aea)
-	geo_100_exsitu <- calc.buff.area(exsitu,100000,wgs.proj,aea.proj,us_boundary.aea)
+		## 100 km buffers
+	geo_100_insitu <- calc.buff.area(insitu,100000,wgs.proj,aea.proj,boundary.aea)
+	geo_100_exsitu <- calc.buff.area(exsitu,100000,wgs.proj,aea.proj,boundary.aea)
 	geo_100_percent <- (geo_100_exsitu/geo_100_insitu)*100
 
 	## Ecological coverage (use count.eco.wwf function if using global ecoregions)
-		# 20 km buffers
-	eco_20_insitu <- count.eco.usl4(insitu,20000,wgs.proj,aea.proj,ecoregions_l4_clean,us_boundary.aea)
-	eco_20_exsitu <- count.eco.usl4(exsitu,20000,wgs.proj,aea.proj,ecoregions_l4_clean,us_boundary.aea)
+		## 20 km buffers
+	eco_20_insitu <- count.eco(insitu,20000,wgs.proj,aea.proj,ecoregions.aea,boundary.aea)
+	eco_20_exsitu <- count.eco(exsitu,20000,wgs.proj,aea.proj,ecoregions.aea,boundary.aea)
 	eco_20_percent <- (eco_20_exsitu/eco_20_insitu)*100
-		# 50 km buffers
-	eco_50_insitu <- count.eco.usl4(insitu,50000,wgs.proj,aea.proj,ecoregions_l4_clean,us_boundary.aea)
-	eco_50_exsitu <- count.eco.usl4(exsitu,50000,wgs.proj,aea.proj,ecoregions_l4_clean,us_boundary.aea)
+		## 50 km buffers
+	eco_50_insitu <- count.eco(insitu,50000,wgs.proj,aea.proj,ecoregions.aea,boundary.aea)
+	eco_50_exsitu <- count.eco(exsitu,50000,wgs.proj,aea.proj,ecoregions.aea,boundary.aea)
 	eco_50_percent <- (eco_50_exsitu/eco_50_insitu)*100
-		# 100 km buffers
-	eco_100_insitu <- count.eco.usl4(insitu,100000,wgs.proj,aea.proj,ecoregions_l4_clean,us_boundary.aea)
-	eco_100_exsitu <- count.eco.usl4(exsitu,100000,wgs.proj,aea.proj,ecoregions_l4_clean,us_boundary.aea)
+		## 100 km buffers
+	eco_100_insitu <- count.eco(insitu,100000,wgs.proj,aea.proj,ecoregions.aea,boundary.aea)
+	eco_100_exsitu <- count.eco(exsitu,100000,wgs.proj,aea.proj,ecoregions.aea,boundary.aea)
 	eco_100_percent <- (eco_100_exsitu/eco_100_insitu)*100
 
 	## Create text for results table
@@ -386,12 +447,9 @@ for(sp in 1:length(target_sp)){
 			geo_avg = geo_avg_txt, eco_avg = eco_avg_txt, stringsAsFactors=F)
   summary_tbl[sp,] <- summary_add
 
-
-	# print progress
-  cat("\tEnding ", target_sp[sp], ", ", sp, " of ", length(target_sp), ".\n\n", sep="")
-
 }
 
-# write summary table
+## write summary table
 summary_tbl
-write.csv(summary_tbl, file.path(output_dir,"ExSituCoverage_Table.csv"),row.names = F)
+write.csv(summary_tbl, file.path(output_dir,"ExSituCoverage_Test_Table.csv"),
+	row.names = F)
